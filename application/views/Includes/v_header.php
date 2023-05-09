83292f3e34c1ffffb85be2de54680215ece7848a
André Luiz Belmonte <belmonte_al@outlook.com>
2023-05-09 15:25:28 -0300

    ultimas alterações da tela de inicio

--- application/views/Includes/v_header.php
+++ application/views/Includes/v_header.php
@@ -15,12 +15,16 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <!-- Required meta tags -->
-    
+
     <script src="https://kit.fontawesome.com/775fd40529.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="//code.jquery.com/jquery-3.6.1.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="icon" type="image/x-icon" href="<?php echo base_url('/assets/img/logos/Johe_icon.png'); ?>" />
+    <link href="<?php echo base_url('/assets/css/styles.css'); ?>" rel="stylesheet" />
+
 
     <!-- this should go after your </body> -->
     <script src="<?php echo base_url('/assets/JS/scripts.js') ?>" crossorigin="anonymous"></script>
@@ -61,18 +65,14 @@
                                     </div>
                                 </li>
                                 <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 mx-2">
-                                    <a class="nav-link" href="#">Consula</a>
+                                    <a class="nav-link" href="#">Consultar</a>
                                     <div class="dropdown-menu">
                                         <a class="dropdown-item" href="#">Material</a>
                                         <a class="dropdown-item" href="<?php echo base_url('/index.php/consulVendas'); ?>">Venda</a>
-                                        <a class="dropdown-item" href="#">Vendedor</a>
                                     </div>
                                 </li>
                                 <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 mx-2">
-                                    <a class="nav-link" href="#">Empresa</a>
-                                </li>
-                                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 mx-2">
-                                    <a class="nav-link" href="#">Contato</a>
+                                    <a class="nav-link" href="<?php echo base_url('/index.php/contato'); ?>">Contato</a>
                                 </li>
                             </ul>
                         </div>
